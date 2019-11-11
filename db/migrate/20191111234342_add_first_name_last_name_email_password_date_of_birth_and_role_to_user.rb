71ba4036963cbd268720a411085df7177223f59b
Joseph Hassett <jchassett818@gmail.com>
2019-11-11 17:51:13 -0600

    fixed migrations and schema for user model with devise

--- db/migrate/20191111234342_add_first_name_last_name_email_password_date_of_birth_and_role_to_user.rb
+++ db/migrate/20191111234342_add_first_name_last_name_email_password_date_of_birth_and_role_to_user.rb
@@ -2,8 +2,6 @@ class AddFirstNameLastNameEmailPasswordDateOfBirthAndRoleToUser < ActiveRecord::
   def change
     add_column :users, :first_name, :string
     add_column :users, :last_name, :string
-    add_column :users, :email, :string
-    add_column :users, :password, :string
     add_column :users, :date_of_birth, :string
     add_column :users, :role, :string
   end
--- db/schema.rb
+++ db/schema.rb
@@ -10,7 +10,7 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 2019_11_11_231627) do
+ActiveRecord::Schema.define(version: 2019_11_11_234342) do
 
   # These are extensions that must be enabled in order to support this database
   enable_extension "plpgsql"
@@ -23,18 +23,12 @@ ActiveRecord::Schema.define(version: 2019_11_11_231627) do
     t.datetime "remember_created_at"
     t.datetime "created_at", null: false
     t.datetime "updated_at", null: false
+    t.string "first_name"
+    t.string "last_name"
+    t.string "date_of_birth"
+    t.string "role"
     t.index ["email"], name: "index_users_on_email", unique: true
     t.index ["reset_password_token"], name: "index_users_on_reset_password_token", unique: true
-  end
-
-  create_table "venues", force: :cascade do |t|
-    t.string "name"
-    t.string "description"
-    t.integer "price_per_hour"
-    t.integer "capacity"
-    t.string "address"
-    t.datetime "created_at", null: false
-    t.datetime "updated_at", null: false
   end
 
 end
