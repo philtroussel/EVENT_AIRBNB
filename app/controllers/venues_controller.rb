10821e9cfe81ed93885c02cffb0b7bf75818142c
Joseph Hassett <jchassett818@gmail.com>
2019-11-12 13:13:25 -0600

    created venues controller and minor edits to new/edit/index views

---
+++ app/controllers/venues_controller.rb
@@ -0,0 +1,44 @@
+class VenuesController < ApplicationController
+  def index
+    @venues = Venue.all
+  end
+
+  def show
+    @venue = Venue.find(params[:id])
+  end
+
+  def new
+    @venue = Venue.new
+  end
+
+  def create
+    find_venue
+    @venue.save
+    redirect_to venue_path(@venue)
+  end
+
+  def edit
+    find_venue
+  end
+
+  def update
+    find_venue
+    @venue.update(venue_params)
+    redirect_to venue_path(@venue)
+  end
+
+  def destroy
+    find_venue
+    @venue.destroy
+  end
+
+  private
+
+  def find_venue
+    @venue = Venue.find(params[:id])
+  end
+
+  def venue_params
+    params.require(:venue).permit(:name, :description, :price_per_hour, :capacity, :address)
+  end
+end
