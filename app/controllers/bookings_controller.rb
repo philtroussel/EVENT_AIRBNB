9dec2fba171f988ad8ef9d249f88cd3a977fa115
Joseph Hassett <jchassett818@gmail.com>
2019-11-13 14:00:00 -0600

    Venue Pundit Complete and working as expected

--- app/controllers/bookings_controller.rb
+++ app/controllers/bookings_controller.rb
@@ -1,11 +1,12 @@
 class BookingsController < ApplicationController
   def index
-    @bookings = Booking.where(user_id: current_user)
+    @bookings = policy_scope(Booking)
   end
 
   def create
     @venue = Venue.find(params[:venue_id])
     @booking = Booking.new()
+    authorize @booking
     @booking.check_in_date_time = "2019-11-#{params[:booking]['check_in_date_time(3i)']} #{params[:booking]['check_in_date_time(4i)']}:00:00"
     @booking.check_out_date_time = "2019-11-#{params[:booking]['check_out_date_time(3i)']} #{params[:booking]['check_out_date_time(4i)']}:00:00"
     @booking.user = current_user
